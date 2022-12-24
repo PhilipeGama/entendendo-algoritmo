1c9f9430edbd2f8090b8134051cf3b413c6784a6
PhilipeGama <philipegama97@gmail.com>
2022-12-23 23:40:58 -0400

    Create selection_sort.py
    cap 2

---
+++ cap-2/selection_sort.py
@@ -0,0 +1,19 @@
+def buscar_menor(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if(arr[i] < menor):
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
+
+def ordenacao_por_selecao(arr):
+    novo_arr = []
+    for i in range(len(arr)):
+        menor = buscar_menor(arr)
+        novo_arr.append(arr.pop(menor))
+    return novo_arr
+
+print(ordenacao_por_selecao([2,1,5,6,4,10,9]))
+
+
